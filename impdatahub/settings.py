78ff78904eb30fd3ef5fed421cb0b9c5c5ddeab5
Vineyald <viniciusmach11@gmail.com>
2024-10-28 15:58:18 -0300

    first fixes - static

--- impdatahub/settings.py
+++ impdatahub/settings.py
@@ -110,6 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'build', 'static'),  # Adiciona os arquivos estáticos do build do React
